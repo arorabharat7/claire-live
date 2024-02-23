f589a0ba96456856b33d888b84d5ac9e4beb545e
vijaysingh177 <vijaysingh@protocloudtechnologies.com>
2024-02-23 15:31:15 +0530

    js in footer

--- wp-content/themes/astra-child/footer.php
+++ wp-content/themes/astra-child/footer.php
@@ -427,7 +427,25 @@ if (!in_array($page_ids, $excluded_idss)) {  ?>
 </footer>
 
 
+<script>
+	let openModalBtn = document.getElementById("openFormBtn");
+	let modal = document.getElementById("myForm");
+	let closeModalBtn = document.getElementById("closeFormBtn");
 
+	openModalBtn.addEventListener("click", function() {
+		modal.style.display = "block";
+	});
+
+	closeModalBtn.addEventListener("click", function() {
+		modal.style.display = "none";
+	});
+
+	window.addEventListener("click", function(event) {
+		if (event.target === modal) {
+			modal.style.display = "none";
+		}
+	});
+</script>
 <?php wp_footer(); ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
